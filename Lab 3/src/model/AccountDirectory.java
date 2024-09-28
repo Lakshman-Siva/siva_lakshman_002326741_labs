dc4e8c5696b9ca23c662172db9d9db5ab971ecff
Lakshman Siva <siva.l@northeastern.edu>
2024-09-28 16:46:11 -0400

    Added code for accountDirectory.java

--- Lab 3/src/model/AccountDirectory.java
+++ Lab 3/src/model/AccountDirectory.java
@@ -12,4 +12,28 @@ import java.util.*;
  */
 public class AccountDirectory {
 	
+	private ArrayList<Account> accounts;
+	
+	public AccountDirectory() {
+		this.accounts = new ArrayList<>();
+	}
+
+	public ArrayList<Account> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(ArrayList<Account> accounts) {
+		this.accounts = accounts;
+	}
+	
+	public Account addAccount() {
+		Account acc = new Account();
+		accounts.add(acc);
+		return  acc;
+	}
+	
+	public void deleteAccount(Account account) {
+		accounts.remove(account);
+	}
+	
 }
